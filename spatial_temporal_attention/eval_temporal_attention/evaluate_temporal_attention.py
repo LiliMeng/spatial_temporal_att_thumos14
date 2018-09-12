2c24dedaf0a47d7bc25aeada20aa77d427e22bd2
LiliMeng <lilimeng1103@gmail.com>
2018-09-12 11:18:27 -0700

    fix bugs

--- spatial_temporal_attention/eval_temporal_attention/evaluate_temporal_attention.py
+++ spatial_temporal_attention/eval_temporal_attention/evaluate_temporal_attention.py
@@ -7,7 +7,7 @@ Date: Sep 11th, 2018
 import numpy as np
 import os
 
-att_weights_dir = "../saved_weights/Contrast_0.0001_TV_reg1e-05_mask_LRPatience5_Adam0.0001_decay0.0001_dropout_0.2_Temporal_ConvLSTM_hidden512_regFactor_1_Sep_11_17_22"
+att_weights_dir = "../saved_weights/Contrast_0.0001_TV_reg1e-05_mask_LRPatience5_Adam0.0001_decay0.0001_dropout_0.2_Temporal_ConvLSTM_hidden512_regFactor_1_Sep_12_11_00"
 
 att_weights_file = os.path.join(att_weights_dir, "test_att_weights.npy")
 test_name_file = os.path.join(att_weights_dir, "test_name.npy")
@@ -27,7 +27,7 @@ for i in range(attention_weights.shape[0]):
 	for j in range(attention_weights.shape[1]):
 		img_index = int(test_img_names[i][j].split('/')[-1].split('.jpg')[0])
 		all_img_index.append(img_index)
-		if attention_weights[i][j] > 0.02:
+		if attention_weights[i][j] > 0.025:
 			good_img_index.append(img_index)
 
 	print("len(good_img_index): ", len(good_img_index))
@@ -45,7 +45,6 @@ for i in range(attention_weights.shape[0]):
 	if IOU > 0.1:
 		IOU_05_count+=1
 
-	if i >2:
-		break
+	
 print("the number of IOU is larger than 0.5 is: ", IOU_05_count)
 print("the percentage of IOU is larger than 0.5 is: ", IOU_05_count/attention_weights.shape[0])
