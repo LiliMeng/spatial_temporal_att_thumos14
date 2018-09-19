949119d99a6fa9ca659aaba10613f6e65a7e38b6
LiliMeng <lilimeng1103@gmail.com>
2018-09-18 17:28:03 -0700

    fix bugs

--- finetune_network_extract_features/thumos14_extract_features.py
+++ finetune_network_extract_features/thumos14_extract_features.py
@@ -211,7 +211,8 @@ def main():
     #self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
     scheduler = ReduceLROnPlateau(optimizer, 'min', patience=1,verbose=True)
 
-    model_resume_path ='./record/spatial/valid_test_best/model_best.pth.tar'
+    #model_resume_path ='./record/spatial/valid_test_best/model_best.pth.tar'
+    model_resume_path = './record/spatial/resnet_ucf101_org/model_best.pth.tar'
     # 2. load pretrained model
     if os.path.isfile(model_resume_path):
         print("==> loading checkpoint '{}'".format(model_resume_path))
@@ -230,12 +231,12 @@ def main():
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
-    annot_img_index_list, entire_img_index_list, all_frames, all_frame_names, all_labels = load_frames(new_img_list = "new_file_with_start_end_frame_test.txt",
-                                            img_list = "./thumos14_list/final_thumos_14_20_one_label_temporal_test_with_img_num.txt",
-                                            video_root_path = "/media/dataDisk/THUMOS14/THUMOS14_video/thumos14_preprocess/test/frames_10fps",
+    annot_img_index_list, entire_img_index_list, all_frames, all_frame_names, all_labels = load_frames(new_img_list = "new_file_with_start_end_frame_valid.txt",
+                                            img_list = "./thumos14_list/final_thumos_14_20_one_label_temporal_valid_with_img_num.txt",
+                                            video_root_path = "/media/dataDisk/THUMOS14/THUMOS14_video/thumos14_preprocess/valid/frames_10fps",
                                             num_frames=30)
 
-    feature_dir = "./saved_features/test"
+    feature_dir = "./saved_features/valid"
 
     if not os.path.exists(feature_dir):
         os.makedirs(feature_dir)
@@ -286,10 +287,11 @@ def main():
         print('video {} done, total {}/{}, moving Prec@1 {:.3f} Prec@5 {:.3f}'.format(i, i+1,
             toal_num_video,top1.avg, top5.avg))
 
-        
-        np.save(os.path.join(feature_dir, 'features_{}.npy'.format('%05d'%i)), features_np)
-        np.save(os.path.join(feature_dir, 'name_{}.npy'.format('%05d'%i)), all_frame_names[i])
-        np.save(os.path.join(feature_dir, 'label_{}.npy'.format('%05d'%i)), per_video_label)
+        #save_num =i+7571
+        save_num = i
+        np.save(os.path.join(feature_dir, 'features_{}.npy'.format('%05d'%save_num)), features_np)
+        np.save(os.path.join(feature_dir, 'name_{}.npy'.format('%05d'%save_num)), all_frame_names[i])
+        np.save(os.path.join(feature_dir, 'label_{}.npy'.format('%05d'%save_num)), per_video_label)
  #    # all_logits = np.asarray(all_logits_list)
  #    # all_frame_names = np.asarray(all_frame_names)
  #    # all_labels = np.asarray(all_labels)
